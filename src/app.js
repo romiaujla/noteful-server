0065696957eaf02a10e8137b44fc0baf66f52839
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-04 15:36:26 -0400

    add code to import folder-router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require ('cors');
 const helmet = require('helmet');
+const folderRouter = require('./folders/folder-router');
 
 const app = express();
 const morganOptions = NODE_ENV === 'production' 
@@ -12,6 +13,8 @@ const morganOptions = NODE_ENV === 'production'
 app.use(morgan(morganOptions));
 app.use(helmet());
 app.use(cors());
+
+app.use('/folders', folderRouter);
 
 app.get(`/`, (_,res)=>{
     res.send(`Hello World`);
