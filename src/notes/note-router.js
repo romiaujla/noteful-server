d27e280ec378d29f8748534d23ba7cf7545ace3e
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-04 15:55:19 -0400

    created DELETE /notes/:id and GET /notes/:id

--- src/notes/note-router.js
+++ src/notes/note-router.js
@@ -14,4 +14,27 @@ noteRouter
             .catch(next);
     })
 
+noteRouter
+    .route('/:id')
+    .get((req, res, next) => {
+        const db = req.app.get('db');
+        const { id } = req.params;
+        NotesServices.getNotesById(db, id)
+            .then((note) => {
+                res.json(note);
+            })
+            .catch(next);
+    })
+    .delete((req, res, next) => {
+        const db = req.app.get('db');
+        const { id } = req.params;
+        NotesServices.deleteNote(db, id)
+            .then((deletedNote) => {
+                res
+                    .status(204)
+                    .end();
+            })
+            .catch(next);
+    })
+
 module.exports = noteRouter;
