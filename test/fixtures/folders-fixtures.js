35c0169f52b50252ccc519d76cbc718a6999ccb4
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-05 15:26:01 -0400

    fixed id in fixtures.

--- test/fixtures/folders-fixtures.js
+++ test/fixtures/folders-fixtures.js
@@ -2,15 +2,15 @@ function makeFoldersArray(){
     return (
         [
             {
-              "id": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+              "id": 1,
               "name": "Important"
             },
             {
-              "id": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+              "id": 2,
               "name": "Super"
             },
             {
-              "id": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
+              "id": 3,
               "name": "Spangley"
             }
           ]
