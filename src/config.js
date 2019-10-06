f002d13742b5355a198c3a51d5c9811ee9589107
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-06 13:08:41 -0400

    added URI for db connection manually

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 8000,
-    DATABASE_URL: process.env.DATABSE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
+    DATABASE_URL: 'postgres://svyxjkjrjbjxrf:06e134c51460f2f4bf791561bc68c776cc4e719fe3ba06dee74a151c1bfade70@ec2-54-235-100-99.compute-1.amazonaws.com:5432/d7e9bm5n9p2mja' || 'postgresql://dunder-mifflin@localhost/noteful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful-test',
 }
