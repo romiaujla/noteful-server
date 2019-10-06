2e1e6f6072c657d064207fe77d2e46c23a194625
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-06 12:58:38 -0400

    corrected typo

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const folderRouter = require('./folders/folder-router');
 const noteRouter = require('./notes/note-router');
 
 const app = express();
-app.use(cors());	
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test'
   }));
 app.use(helmet());
+app.use(cors());	
 
 
 app.use('/folders', folderRouter);
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 8000,
-    DATABSE_URL: process.env.DATABSE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
+    DATABASE_URL: process.env.DATABSE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful-test',
 }
